c6e434fcfa807a90d79720a8fab73e4a864b8d8e
0xw01f <76235339+0xw01f@users.noreply.github.com>
2021-03-13 19:26:37 +0100

    Implementation of app layouts

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>About us | Hello City</title>
+@section('content')
+    
 
-    </head>
-    <body>
-        
-        <p>Built with &hearts; by W01f.</p>
+    <p>Built with &hearts; by W01f.</p>
 
-        <p><a href="/"> Go to homepage </a></p>
-      
+    <p><a href="/"> Go to homepage </a></p>
+    
 
-      <footer>
-          <p>&copy; Copyright {{ date('Y') }}</p>
-      </footer>
-    </body>
-</html>
+@endsection
