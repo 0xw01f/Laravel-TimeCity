44083b0bcaf9b171e5befec32550e77da1898d24
0xw01f <76235339+0xw01f@users.noreply.github.com>
2021-03-14 00:52:28 +0100

    Test

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,7 +7,7 @@
     
     <img src="{{ asset('/img/w01f-logo.png') }}" alt="W01f logo" class="h-64 my-12 rounded-full shadow-md">
 
-    <h2 class="mb-5 text-grey-700">Built with <span class="text-pink-500">&hearts;</span> by W01f.</h2>
+    <h2 class="mb-5 text-grey-700">Built with <span class="text-pink-500">&hearts;</span> by W01f. huhu</h2>
 
     <p><a href="{{ route('home')}}" class="text-indigo-500 hover:text-indigo-600"> Go to homepage </a></p>
     
