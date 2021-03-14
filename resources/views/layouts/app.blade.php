294ecd92eba0925b90694464b452528f57b24335
0xw01f <76235339+0xw01f@users.noreply.github.com>
2021-03-14 15:58:58 +0100

    Replaced tailwind styles from CDN by a real installation, and added dark mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,15 +6,24 @@
 
         <title> {{ page_title($title ?? null) }}</title>
 
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     </head>
-    <body class="py-6 flex flex-col justify-between items-center min-h-screen">
+    <body class="py-6 flex flex-col justify-between items-center min-h-screen dark:bg-gray-800">
+        
         <main role="main" class="flex flex-col justify-center items-center">
             @yield('content')
         </main>
 
         @include('layouts/partials/_footer')
 
+    <script src="{{ asset('js/app.js') }}" defer> </script>
+    
+    <script>
+       
+    </script>
+
     </body>
+
+
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,11 +3,11 @@
 
 
 @section('content')
-
+      
       <img src="{{ asset('/img/IcelandFlagMap.png') }}" alt="Iceland Flag" class="h-32 rounded shadow-md">
 
       <h1 class="mt-5 text-4xl sm:text-5xl text-indigo-600 font-semibold">Hello from Iceland!</h1>
 
-      <p class="text-lg text-gray-800">It's currently {{ date('h:i A') }}</p>
+      <p class="text-lg text-gray-500 dark:text-gray-300 ">It's currently {{ date('h:i A') }}</p>
       
 @endsection
