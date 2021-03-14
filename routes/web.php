09c0ec09f3e0c088451c65c7fd25c84602c30ea9
0xw01f <76235339+0xw01f@users.noreply.github.com>
2021-03-14 01:57:58 +0100

    Correction in about route

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'pages/home')->name('home');
 
-Route::view('/', 'pages/about')->name('about');
+Route::view('/about-me', 'pages/about')->name('about');
